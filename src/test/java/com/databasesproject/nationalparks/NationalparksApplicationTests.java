60e05aecb0df1a8323f96f474758a06f3b591b9d
hedgehogdrfrink <pradeepbhatnerd@protonmail.com>
2024-10-31 21:03:14 -0400

    first push

---
+++ src/test/java/com/databasesproject/nationalparks/NationalparksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.databasesproject.nationalparks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NationalparksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
